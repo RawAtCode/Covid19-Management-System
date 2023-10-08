9531cc6cbd3d00ea206265b4c59e161f78ceaa59
Sandeep Rawat <95517890+RawAtCode@users.noreply.github.com>
2023-10-08 11:18:29 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+ <footer class="sticky-footer bg-white">
+     <div class="container my-auto">
+         <div class="copyright text-center my-auto">
+             <span>Copyright &copy; 2021 <br> Developed by Sandeep Rawat.</span>
+         </div>
+     </div>
+ </footer>
+ <!-- End of Footer -->
